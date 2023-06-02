300c1d6a4eba828fda366ffd5fe3d46f694083bb
SlavikDemian <slavikdemian@gmail.com>
2023-06-02 18:37:13 +0300

    DZ 4 _1

---
+++ DZ_4.js
@@ -0,0 +1,135 @@
+let userAge = prompt('Введите свой год рождения')
+let userCiti = prompt ('Введите город с котором Вы живете')
+let userSport = prompt('Введите любимый вид спорта')
+let nowYear = 2023
+// let userCitiCapital
+
+let citiFirst = 'Київ'
+let citiSecond = 'Вашингтон'
+let citiThird = 'Лондон'
+let sportFirst = 'Греко-римська боротьба'
+let sportSecond = 'Футбол'
+let sportThird = 'Бокс'
+let sportFirstAthlete = 'Беленюк Жан'
+let sportSecondAthlete = 'Шевченко Андрій'
+let sportThirdAthlete = 'Олександр Усик'
+// let ageOfUserStri = nowYear - userAge
+
+
+
+if (userCiti === citiFirst || userCiti === citiSecond || userCiti === citiThird){
+    userCitiCapital = `Ти живеш у столиці ${userCiti}`
+    console.log(`Ти живеш у столиці ${userCiti}`)
+    if (userSport === sportFirst){
+        sportStruggle = `Круто! Хочеш стати ${sportFirstAthlete}`
+        console.log(`Круто! Хочеш стати ${sportFirstAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiCapital}  ${sportStruggle}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiCapital}  ${sportStruggle}`)
+        }
+    } else if(userSport === sportSecond) {
+        sportFootball = `Круто! Хочеш стати ${sportSecondAthlete}`
+        console.log(`Круто! Хочеш стати ${sportSecondAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiCapital}  ${sportFootball}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiCapital}  ${sportFootball}`)
+        }
+    } else if(userSport === sportThird) {
+        sportBox = `Круто! Хочеш стати ${sportThirdAthlete}`
+        console.log(`Круто! Хочеш стати ${sportThirdAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiCapital}  ${sportBox}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiCapital}  ${sportBox}`)
+        }
+    } else {
+        userSportCancel = 'Шкода, що Ви не захотіли ввести свій(ю) спорт'
+    }
+} else if (userCiti) {
+    userCitiLive = `Ти живеш у місті ${userCiti}`
+    console.log(`Ти живеш у місті ${userCiti}`)
+    if (userSport === sportFirst){
+        sportStruggle = `Круто! Хочеш стати ${sportFirstAthlete}`
+        console.log(`Круто! Хочеш стати ${sportFirstAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiLive}  ${sportStruggle}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiLive}  ${sportStruggle}`)
+        }
+    } else if(userSport === sportSecond) {
+        sportFootball = `Круто! Хочеш стати ${sportSecondAthlete}`
+        console.log(`Круто! Хочеш стати ${sportSecondAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiLive}  ${sportFootball}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiLive}  ${sportFootball}`)
+        }
+    } else if(userSport === sportThird) {
+        sportBox = `Круто! Хочеш стати ${sportThirdAthlete}`
+        console.log(`Круто! Хочеш стати ${sportThirdAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiLive}  ${sportBox}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiLive}  ${sportBox}`)
+        }
+    } else {
+        userSportCancel = 'Шкода, що Ви не захотіли ввести свій(ю) спорт'
+    }
+} else if (userCiti === null) {
+    userCitiCancel = 'Шкода, що Ви не захотіли ввести свій(ю) місто'
+    console.log(`Шкода, що Ви не захотіли ввести свій(ю) місто`)
+    if (userSport === sportFirst){
+        sportStruggle = `Круто! Хочеш стати ${sportFirstAthlete}`
+        console.log(`Круто! Хочеш стати ${sportFirstAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiCancel}  ${sportStruggle}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiCancel}  ${sportStruggle}`)
+        }
+    } else if(userSport === sportSecond) {
+        sportFootball = `Круто! Хочеш стати ${sportSecondAthlete}`
+        console.log(`Круто! Хочеш стати ${sportSecondAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiCancel}  ${sportFootball}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiCancel}  ${sportFootball}`)
+        }
+    } else if(userSport === sportThird) {
+        sportBox = `Круто! Хочеш стати ${sportThirdAthlete}`
+        console.log(`Круто! Хочеш стати ${sportThirdAthlete}`)
+        if (userAge) {
+            ageOfUserStri = nowYear - userAge
+            alert(`Тобі ${ageOfUserStri} років ${userCitiCancel}  ${sportBox}`)
+        } else {
+            userAgeCancel = 'Шкода, що Ви не захотіли ввести свій(ю) вік'
+            alert(`${userAgeCancel}  ${userCitiCancel}  ${sportBox}`)
+        }
+    } else {
+        userSportCancel = 'Шкода, що Ви не захотіли ввести свій(ю) спорт'
+    }
+} else {
+    console.log('Что-то не так')
+}
+
+
+// console.log(userCitiCapital)
+// console.log(userCitiLive)
+// console.log(userCitiCancel)
--- DZ_5.js
+++ DZ_5.js
@@ -14,20 +14,21 @@
 // console.log(isNaN(+numOrStr))
 
 
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
 
-switch (true) {
-    case numOrStr === null:
-        alert('ви відмінили');
-        break;
-    case numOrStr.trim() === '':
-        alert('Empty String')
-        break;
-    case isNaN(+numOrStr):
-        alert('number is Ba_NaN');
-        break;
-    default:
-        alert('OK!');
-}
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+//
+// switch (true) {
+//     case numOrStr === null:
+//         alert('ви відмінили');
+//         break;
+//     case numOrStr.trim() === '':
+//         alert('Empty String')
+//         break;
+//     case isNaN(+numOrStr):
+//         alert('number is Ba_NaN');
+//         break;
+//     default:
+//         alert('OK!');
+// }
 
