e0186aab42dc374b66ac72bf9a1b5796f9186887
SlavikDemian <slavikdemian@gmail.com>
2023-06-18 17:21:36 +0300

    DZ 14 end

---
+++ DZ_14.js
@@ -0,0 +1,10 @@
+
+function pow(num,degree) {
+    if (degree != 1) {
+        return num = num * pow(num,degree - 1);
+    } else {
+        return num;
+    }
+}
+const result = pow(10,3);
+console.log(result)
