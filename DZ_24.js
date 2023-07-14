54cb687ff14a8b8666b1af1d64d3d10096ee6cb9
SlavikDemian <slavikdemian@gmail.com>
2023-07-14 17:35:25 +0300

    DZ 24 end _2

--- DZ_24.js
+++ DZ_24.js
@@ -1,29 +1,15 @@
 
-
 const inputElem = document.getElementsByTagName('input')
-inputElem[0].setAttribute('onfocus', 'myFunction1()');
-inputElem[0].setAttribute('onblur', 'myFunction2()');
-inputElem[0].setAttribute('id', 'inputID');
-const inputID = document.getElementById('inputID')
 
-
-let divID;
-function myFunction1(){
-    const divTag = document.createElement('div')
-    divTag.setAttribute('id', 'divID')
+let divTag;
+    inputElem[0].addEventListener('focus', () => {
+    divTag = document.createElement('div')
     document.body.appendChild(divTag)
-    divID = document.getElementById('divID')
-
-
     const text = document.createTextNode('при фокусировки появляется сбоку div')
-    divID.appendChild(text)
-    divID.setAttribute('style', 'display: inline-block')
-
-
-    document.body.appendChild(divID)
-
-}
-function myFunction2() {
-    divID.remove();
-}
+    divTag.appendChild(text)
+    divTag.setAttribute('style', 'display: inline-block')
+});
+    inputElem[0].addEventListener('blur', () => {
+    divTag.remove();
+})
 
